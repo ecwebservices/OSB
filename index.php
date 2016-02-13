8471ed6840f7d08d0a67fd2ceaca4d582402dea8
ECWebServices <ecwebservices@gmail.com>
2016-02-13 20:30:15 +0000

    fixed

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php $pt = "Home"; ?>
-<?php include 'header.php';?>
+<?php include './livesite/header.php';?>
 # Welcome to OSB!
 
 OSB (Open Source Bible) Is the best place to get your scripture!
 
 Our site is currently not ready yet, so be prepared!
-<?php include 'footer.php';?>
+<?php include './livesite/footer.php';?>
